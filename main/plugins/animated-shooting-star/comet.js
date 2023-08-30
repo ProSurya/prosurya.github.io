0cc2b2669eda1b7fa3fe8472942dd3fa4e2d6e6c
ProSurya <112009158+ProSurya@users.noreply.github.com>
2023-08-30 19:22:18 +0700

    Update comet.js

--- main/plugins/animated-shooting-star/comet.js
+++ main/plugins/animated-shooting-star/comet.js
@@ -3,7 +3,7 @@ const cover = document.querySelector('#cover')
 let cometCount = ""
 // for(let c = 0; c < 3; c++) {
 //     cometCount += `<div class="shootingStarContainer">`
-    for(let i = 0; i < 10; i++) {
+    for(let i = 0; i < 1; i++) {
         // cometCount += `<div class="shootingStar"></div>`
         cometCount += `<div class="comet"></div>`
     }
